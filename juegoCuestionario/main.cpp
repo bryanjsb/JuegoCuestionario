e36b7833fbb0a700c7d5ea414514dd4905d6d265
Bryan Sánchez Brenes <bryan.jsb.1801.@gmail.com>
2022-08-14 18:13:03 -0600

    ingresando jugador, archivos completo

--- juegoCuestionario/main.cpp
+++ juegoCuestionario/main.cpp
@@ -4,6 +4,7 @@
 #include <conio.h>
 #include <iomanip>
 #include <cctype>
+#include <fstream>
 
 using namespace std;
 
@@ -17,8 +18,9 @@ void listaMejoresJugadores();
 void salirPrograma();
 
 //Archivos - jugadores
-void guardarJugadores();
+void guardarJugadores(int noCedula, string nombre);
 void cargarJugadores();
+bool validarJugador(int noCedula);
 
 //Archivos - preguntas
 
@@ -27,6 +29,9 @@ void jugar();
 
 //validaciones
 bool validarCedula(int cedula);
+int obtenerEntero(istream &linea);
+double obtenerFlotante(istream &linea);
+string obtenerHilera(istream &linea);
 
 int main() {
     srand(time(NULL));
@@ -99,26 +104,108 @@ void ingresarJugadores() {
     cout<<"Nombre jugador: ";
     cin>> nombre;
     cout<<endl;
+
+    guardarJugadores(noCedula,nombre);
+
+}
+
+void guardarJugadores(int noCedula, string nombre) {
+    if(!validarJugador(noCedula)){
+
+    try {
+        cout<<"-----Guardando informacion registro Jugadores-----"<<endl;
+        //el ios::app  sirve para abrir un fichero de salida en modo añadir,
+        //de manera que no borra el fichero actual y el cursor del fichero se pone después del último elemento.
+        ofstream os("jugadoresPrueba.txt", ios::out | ios::app);// Creando y abriendo archivo
+        if (!os) {
+            throw exception();
+        } else {
+            os<<noCedula;
+            os<<"-";
+            os<<nombre;
+            os<<"-0"<<endl;
+            os.close();
+        }
+    } catch(...) {
+        cout << "ERROR :Algo paso con la escritura..." << endl;
+    }
+
+    try {
+        ofstream os("registroCedulaJugador.txt", ios::out | ios::app);
+        if (!os) {
+            throw exception();
+        } else {
+            os<<noCedula<<endl;
+            os.close();
+        }
+    } catch(...) {
+        cout << "ERROR :Algo paso con la escritura..." << endl;
+    }
+    }
+}
+
+bool validarJugador(int noCedula) {
+    long double cedulas[100];
+
+    try {
+        //cout << "------Cargando Informacion Jugadores------" << endl;
+
+        ifstream is("registroCedulaJugador.txt", ios::in);
+        if (!is) {
+            throw exception();
+        } else {
+            int i=0;
+            int cedula;
+            is >> cedula;
+            while(!is.eof()) {
+                cedulas[i] = cedula;
+                i++;
+                is >> cedula;
+            }
+            is.close();
+        }
+    } catch(...) {
+        cout << "ERROR: Algo paso " << endl;
+    }
+
+    //comparando si existe la cedula en el registro
+    for(int i = 0; i<100;i++){
+        if(cedulas[i]==noCedula){
+            return false;
+        }
+    }
+    return true;
+
 }
 
 int obtenerCedula() {
-    string noCedula;
+    //https://ccia.ugr.es/~jfv/ed1/c/cdrom/cap2/cap24.htm
+    // unsigned elimina numero negativos
+    // long int tengo mayor rango
+    long double noCedula;
 
     try {
         cout<<"Numero de cedula: ";
         cin>>noCedula;
         cout<<endl;
 
-        if(0) {
-            throw(1) ;
+        if(!(noCedula>=100000000 && noCedula<=999999999)) {
+            throw(1);
         }
 
     } catch(int x) {
-        cerr<<"ERROR EL NUMERO DE CEDULA ES INCORRECTO,  TIPO DE ERROR "<<x<<endl;
+        cerr<<"ERROR EL NUMERO DE CEDULA ES INCORRECTO,  TIPO DE ERROR: "<<x<<endl;
         obtenerCedula();
     }
 
-
+    try{
+        if(!validarJugador(noCedula)){
+            throw(2);
+        }
+    }catch(int x){
+        cerr<<"EL JUGADOR YA EXISTE, TIPO DE ERROR: "<<x<<endl;
+        obtenerCedula();
+    }
 
     return noCedula;
 }
@@ -141,5 +228,53 @@ void salirPrograma() {
 }
 void jugar() {
     cout<<"Jugar"<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int obtenerEntero(istream &linea) {
+    string s = obtenerHilera(linea);
+    stringstream r(s);
+    int n;
+    if (!(r >> n)) {
+        throw -1;
+    }
+    return n;
+}
+
+double obtenerFlotante(istream &linea) {
+    string s = obtenerHilera(linea);
+    stringstream r(s);
+    double x;
+    if (!(r >> x)) {
+        throw -1;
+    }
+    return x;
+}
+
+string obtenerHilera(istream &linea) {
+    string r;
+    getline(linea, r, '\t');
+    return r;
 }
 
